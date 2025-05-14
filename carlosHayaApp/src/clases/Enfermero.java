37e8b4df5815c1a4f6fe6130f24c6abf8763e80f
Piotrek Budzowski <piobudzows@gmail.com>
2025-05-14 20:36:23 +0200

    Create Enfermero.java

---
+++ carlosHayaApp/src/clases/Enfermero.java
@@ -0,0 +1,16 @@
+package clases;
+
+public class Enfermero extends Empleado {
+    public Enfermero(String nombre, String DNI, String rol, String turno) {
+        super(nombre, DNI, rol, turno);
+    }
+
+    // POR DESAROLLAR
+    public void asignarCama(Paciente paciente, Habitacion habitacion) {
+
+    }
+
+    public void marcarAltaMedica(Paciente paciente) {
+
+    }
+}
