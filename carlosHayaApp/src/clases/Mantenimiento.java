cb8c2bf60a1526c7361fa01a74d92ec05c482d8e
Piotrek Budzowski <piobudzows@gmail.com>
2025-05-14 20:36:29 +0200

    Create Mantenimiento.java

---
+++ carlosHayaApp/src/clases/Mantenimiento.java
@@ -0,0 +1,12 @@
+package clases;
+
+public class Mantenimiento extends Empleado {
+    public Mantenimiento(String nombre, String DNI, String rol, String turno) {
+        super(nombre, DNI, rol, turno);
+    }
+
+    // POR DESAROLLAR
+    public void realizarMantenimiento(Sala sala) {
+
+    }
+}
