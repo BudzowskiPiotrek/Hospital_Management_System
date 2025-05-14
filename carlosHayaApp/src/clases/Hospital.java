47b899b86898555cae46b72eb84ab3b7dce44268
Piotrek Budzowski <piobudzows@gmail.com>
2025-05-14 20:36:28 +0200

    Create Hospital.java

---
+++ carlosHayaApp/src/clases/Hospital.java
@@ -0,0 +1,21 @@
+package clases;
+
+public class Hospital {
+    private String nombre;
+    private String direccion;
+
+    public Hospital(String nombre, String direccion) {
+        this.nombre = nombre;
+        this.direccion = direccion;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+
+}
